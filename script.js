2ca5a8a2fd659cd5318b874d8e5a4463cb217aeb
nehaha7 <nehabbas4@gmail.com>
2022-03-01 21:07:11 -0800

    shit added

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-function runCode()
+// 32 ordle = duovigentiordle
+
+//green rgb(128,166,108)
+//red rgb(221,103,97)
+//yellow rgb(242,170,80)
+
+var x;
+var tries;
+
+function start()
 {
-    alert("hey bbgirl <33");
+    x = document.getElementById("x=").value;
+    if (x == '')
+        alert("please enter a # !!");
+    else 
+    {
+        window.location.href='play.html';
+
+        //add x to local storage to use across files
+        localStorage.setItem("xordle", x);
+
+        //# of spaces in a wordle is always the x value + 5;
+        //i.e. octordle has 13 guesses
+        tries = parseInt(x) + 5;
+        localStorage.setItem("tries", tries);
+        //alert(localStorage.getItem("tries"));
+
+    }
+        
+
+    
+        
 }
+
